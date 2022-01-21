c42b554ed4a4cc52855a489ffc03f9d8da657230
Sanjay Anuchuri <sanjay.anuchuri@gmail.com>
2022-01-21 23:35:17 +0530

    initial commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/Header.css'
+
+const Header = () => {
+  return (
+  <div className='header'>
+    <h6>easy chat</h6>
+  </div>
+  )
+}
+
+export default Header
